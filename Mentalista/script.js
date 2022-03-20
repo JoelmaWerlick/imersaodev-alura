799ccfc876e871364604ed2b46e18efbb5947166
JoelmaWerlick <100633937+JoelmaWerlick@users.noreply.github.com>
2022-03-20 16:59:08 -0300

    Add files via upload
    Na terceira aula da Imersão Dev, criamos um jogo de adivinhação, onde o programa escolhe um número aleatório e nós tentamos acertar.

---
+++ Mentalista/script.js
@@ -0,0 +1,20 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+  var elementoResultado = document.getElementById("resultado");
+  var chute = parseInt(document.getElementById("valor").value);
+
+  if (numeroSecreto > chute) {
+    var dica = " É mais que isso...";
+  } else {
+    var dica = " É menor que isso";
+  }
+
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "Você acertou!";
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
+  } else {
+    elementoResultado.innerHTML = "OPS...Tente de novo " + dica;
+  }
+}
